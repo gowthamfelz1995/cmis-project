0c694d50d82ef5aa6c557952e4fc99902f50ee07
gowthamaspi <gowtham@aspigrow.com>
2020-06-26 15:40:35 +0530

    Added Toast Message

--- force-app/main/default/lwc/servicePlanning/servicePlanning.js
+++ force-app/main/default/lwc/servicePlanning/servicePlanning.js
@@ -1,24 +1,14 @@
 import { LightningElement, wire,track } from 'lwc';
-import SERVICE_RECORD from '@salesforce/schema/AG_Service__c';
-import SERVICE_TYPE_FIELD from '@salesforce/schema/AG_Service__c.AG_Service_Type__c';
-import { getPicklistValuesByRecordType,getPicklistValues, getObjectInfo } from 'lightning/uiObjectInfoApi'; 
 import { NavigationMixin } from 'lightning/navigation';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 export default class ServicePlanning extends NavigationMixin(LightningElement) {
-    fromDate = ''
-    toDate = ''
-    treeModel;
-    error;
-    recordTypeIdValue = ''
-    serviceObj = SERVICE_RECORD
-    serviceId;
     refferalId = 'a026D000002cDGhQAM';
     handleSuccess(event) {
-        this.serviceId = event.detail.id;
-        console.log("this.serviceId-->"+this.serviceId)
+        this.showNotification();
         this[NavigationMixin.Navigate]({
             type: 'standard__recordPage',
             attributes: {
-                recordId:  this.serviceId,
+                recordId:event.detail.id,
                 objectApiName: 'AG_Service__c',
                 actionName: 'view'
             }
@@ -35,51 +25,12 @@ export default class ServicePlanning extends NavigationMixin(LightningElement) {
             }
         });
     }
-
-    @wire(getPicklistValues, {
-        recordTypeId: '012000000000000AAA',
-        fieldApiName: SERVICE_TYPE_FIELD
-    })
-    picklistValues({ error, data }) {
-            if (data) {
-                console.log('data-->'+JSON.stringify(data));
-            } else {
-                console.log('error-->'+error);
-                }
-        }
-        handleFormSubmitted(event){
-   
-        }
-
-    // buildTreeModel(picklistValues) {
-    //     const treeNodes = [];
-    //     Object.keys(picklistValues).forEach((picklist) => {
-    //         treeNodes.push({
-    //             label: picklist,
-    //             items: picklistValues[picklist].values.map((item) => ({
-    //                 label: item.label,
-    //                 name: item.value
-    //             }))
-    //         });
-    //     });
-    //     return treeNodes;
-    // }
-    // GET PICKLIST VALUES 
-    // @wire (getPicklistValues, {recordTypeId: '$objectInfo.defaultRecordTypeId', fieldApiName: SERVICE_TYPE_FIELD})
-    // picklistValues({
-    //     error,data
-    // })
-    // {if(data){
-    //     this.picklistValues = data;
-    // } else if(error){
-    //     console.log(error);
-    // }} 
-   
-
-    handleFromDateChanges(event){
-        fromDate = event.target.value ;
-    }
-    handleToDateChanges(event){
-        toDate = event.target.value ;
+    showNotification() {
+        const evt = new ShowToastEvent({
+            title: this.titleText,
+            message: 'Service created successfully!',
+            variant: 'success'
+        });
+        this.dispatchEvent(evt);
     }
 }
